72ef971c39d24c77a51fb5fa93040df03dbad963
Zachary Schulz <zjschulz@gmail.com>
2019-12-15 18:59:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,6 +23,11 @@ class App < Sinatra::Base
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
     @w1 = params[:word1]
+    @w2 = params[:word2]
+    @w3 = params[:word3]
+    @w4 = params[:word4]
+    @w5 = params[:word5]
+    "#{@w1}"
   end
 
 end
